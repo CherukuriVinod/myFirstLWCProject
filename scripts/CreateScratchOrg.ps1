07f67b232f6803537135f2e679874ecdab32a104
cherukurivinod <vinod.cherukuris@gmail.com>
2021-08-19 00:46:22 -0400

    Small Changes

--- scripts/CreateScratchOrg.ps1
+++ scripts/CreateScratchOrg.ps1
@@ -1,3 +1,4 @@
 sfdx force:org:create -s -f config\project-scratch-def.json --setalias saraDanPlural --durationdays 7 --setdefaultusername
 sfdx force:source:push
 sfdx force:user:permset:assign -n Source_Tracker
+sfdx force:org:open
