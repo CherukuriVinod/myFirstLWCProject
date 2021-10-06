1a4e226a4f21713cad7251790dca7f3bc76c071d
cherukurivinod <vinod.cherukuris@gmail.com>
2021-10-06 01:47:56 -0400

    Script Change

--- scripts/CreateScratchOrg.ps1
+++ scripts/CreateScratchOrg.ps1
@@ -1,14 +1,17 @@
 echo "Create a scratch org"
 sfdx force:org:create -s -f config\project-scratch-def.json --setalias saraDanPlural --durationdays 7 --setdefaultusername
 
-echo "Push source if present"
+echo "Push source"
 sfdx force:source:push
 
-echo "Assign permission set if present"
+echo "Assigning permission set Source Tracker"
 sfdx force:user:permset:assign -n Source_Tracker
+
+echo "Pushing Data To Product Object"
+sfdx force:data:tree:import -u saraDanPlural --plan data/Product2-plan.json
 
 echo "Open Newly Created Scratch Org"
 sfdx force:org:open
 
 echo "Show Org List"
-sfdx force:org:list
+sfdx force:org:list --all
